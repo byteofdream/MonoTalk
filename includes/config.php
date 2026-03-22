60b6bbd3a7c0f45524c1b3a9cdfa64950b69a936
Говноед <govnoed148852228@gmail.com>
2026-03-22 19:32:22 +0300

    вотч дем вотч демо

--- includes/config.php
+++ includes/config.php
@@ -4,6 +4,8 @@
  * Для хостинга: измените BASE_URL на путь к проекту (например /MonoTalkV2/)
  */
 
+date_default_timezone_set(getenv('APP_TIMEZONE') ?: 'UTC');
+
 define('BASE_URL', '/');
 define('UPLOAD_DIR', __DIR__ . '/../uploads/');
 // Ссылка на репозиторий проекта (замените на свой)
